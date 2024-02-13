8882582c1ea288cbe78681d8fe1c04bb2e93e99d
rocoriyan <rcorrigan.other+github@gmail.com>
2024-02-13 18:17:43 +0000

    completed setup

--- src/books/controllers.js
+++ src/books/controllers.js
@@ -13,4 +13,8 @@ const addBook = async (req, res) => {
     catch(error){
         res.status(500).json({ message: error.message, error: error });
     }
-}
+};
+
+module.exports = {
+    addBook: addBook,
+};
--- src/server.js
+++ src/server.js
@@ -3,11 +3,15 @@ const express = require("express");
 
 const Book = require("./books/model");
 
+const bookRouter = require("./books/routes")
+
 const port = process.env.PORT || 5001;
 
 const app = express();
 
 app.use(express.json());
+
+app.use(bookRouter);
 
 const syncTables = async () => {
     await Book.sync();
@@ -16,6 +20,7 @@ const syncTables = async () => {
 app.get("/health", (req, res) =>{
     res.status(200).json({ message: "API is healthy" });
 })
+
 
 app.listen(port, () => {
     syncTables();
